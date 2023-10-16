f7966a458fb46561fc8fb215779c7a57d599e65d
Hardikk <kambojpushkal@gmail.com>
2023-10-16 21:02:04 +0530

    intital push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import faceimage from './images/image-victor.jpg';
+import CardPattern from './images/bg-pattern-card.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+  <div className="container">
+        <div className="card">
+          <div className="card__header">
+            <img src={CardPattern} alt="Background Pattern" className="bg-card" />
+            <img src={faceimage} alt="Victor's Image" className="card-face" />
+            <div className="content">
+            <div className="intro">
+              <span className="head">Victor Crest</span>
+              <span className="number"> 26</span>
+              <p className="location">London</p>
+            </div>
+            <hr />
+            <div className="stats">
+              <div className="stats1">
+                <ul>
+                  <li className="stats-head">80K</li>
+                  <li className="stats-txt1 stats-txt">Followers</li>
+                </ul>
+              </div>
+              <div className="stats2">
+                <ul>
+                  <li className="stats-head">803K</li>
+                  <li className="stats-txt2 stats-txt">Likes</li>
+                </ul>
+              </div>
+              <div className="stats3">
+                <ul>
+                  <li className="stats-head">1.4K</li>
+                  <li className="stats-txt3 stats-txt">Photos</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
